5509578eeb6340c746ae881c573741c76166d778
Liuqing650 <1006822621@qq.com>
2019-07-05 10:47:52 +0800

    测试ts 类型

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -4,10 +4,14 @@ import * as styles from './index.less';
 export interface HelloProps { compiler: string; framework: string };
 
 export class Hello extends React.Component<HelloProps, {}> {
+  handleClick =  ((name: object) => {
+    console.log('hello------->', name);
+  });
   render() {
     return (
       <div className={styles.wrap}>
-        <h1>Hello from {this.props.compiler} and {this.props.framework} hoho!</h1>
+        <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+        <button className={styles.btn} onClick={this.handleClick.bind(this, {name: 'Tome'})}>Test Button</button>
       </div>
     );
   }
--- src/index.less.d.ts
+++ src/index.less.d.ts
@@ -1,1 +1,2 @@
 export const wrap: string;
+export const btn: string;
